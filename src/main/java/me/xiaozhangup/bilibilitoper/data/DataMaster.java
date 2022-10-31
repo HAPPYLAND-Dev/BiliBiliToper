c32aab1cffddab8a4f2be349ca6be6ed2e72aaff
xiaozhangup <68374769+xiaozhangup@users.noreply.github.com>
2022-10-31 09:52:29 +0800

    我操，严重bug

--- src/main/java/me/xiaozhangup/bilibilitoper/data/DataMaster.java
+++ src/main/java/me/xiaozhangup/bilibilitoper/data/DataMaster.java
@@ -20,7 +20,9 @@ public class DataMaster {
     //视频提交数据的存储
     public static void addPostedVideo(Player p, String bvid) {
         String path = p.getUniqueId().toString();
-        ConfigManager.writeConfig("videos", path, ConfigManager.getConfig("videos").getStringList(path).add(bvid));
+        var list = ConfigManager.getConfig("videos").getStringList(path);
+        list.add(bvid);
+        ConfigManager.writeConfig("videos", path, list);
     }
 
     public static int getTotalVideo(Player p) {
